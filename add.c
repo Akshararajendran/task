cf1ab387450f4d79b3c6674f6c68aa61b9fb6887
Akshararajendran <akshara.rajendran@vvdntech.in>
2019-01-28 18:03:24 +0530

    2

--- add.c
+++ add.c
@@ -1,8 +1,14 @@
 #include<stdio.h>
+int add(int x,int y)
+{
+int c;
+c=x+y;
+return c;
+}
 int main()
 {
-int a,b,c;
-c=a+b;
-printf("%d",c);
+int a,b;
+scanf("%d%d",&a,&b);
+printf("%d",add(a,b));
 return 0;
-}[A
+}
